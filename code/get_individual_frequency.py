d0c79e067fb10bf3f0b25dd3691d74c59355a9e4
Pearson Henri <phenri@palantir.com>
2017-03-08 00:02:20 -0500

    rename directories

--- code/get_individual_frequency.py
+++ code/get_individual_frequency.py
@@ -2,14 +2,16 @@ import csv
 import fileinput
 import os
 
-csv_dir = "/Users/phenri/personal-github/text_with_trump/corpora/speeches/wordcounts/"
+csv_dir = "/Users/phenri/personal-github/text_with_trump/data/wordcounts/"
 
 for wordcounts in os.listdir(csv_dir):
     with open(os.path.join(csv_dir, wordcounts), 'r') as f:
         total_words = 0
+        total_individual_words = 0
         rows = list(csv.reader(f))
         for row in rows:
             total_words += int(row[1])
+            total_individual_words += 1
     new_file = fileinput.input(os.path.join(csv_dir, wordcounts), inplace=True)
     for line in new_file:
         word_count = line.split(",")[1]
