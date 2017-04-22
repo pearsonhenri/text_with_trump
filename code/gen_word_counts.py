18b28c9a3b9b47712e737dc56f59b315127bc6ac
Pearson Henri <phenri@palantir.com>
2017-04-22 19:38:34 -0400

    cleanup gen_word_counts

--- code/gen_word_counts.py
+++ code/gen_word_counts.py
@@ -19,6 +19,35 @@ def clean_speech_file_text(person, filename):
     return cleaned_speech_text
 
 
+def get_counts():
+    for person in os.listdir(corpus_dir):
+        speech_text = ""
+        for raw_speech in os.listdir(os.path.join(corpus_dir, person)):
+            if not (raw_speech.startswith(".DS_Store")):
+                speech_text += " " + clean_speech_file_text(person, raw_speech)
+        speech_words = speech_text.lower().split(" ")
+        word_counts = Counter(speech_words)
+        csv_name = person.lower() + ".csv"
+        writefile = open(os.path.join(csv_dir, csv_name), 'wb')
+        writer = csv.writer(writefile)
+        for key, count in word_counts.iteritems():
+            writer.writerow([key, count])
+        writefile.close()
+
+
+def remove_null_count():
+    for csv in os.listdir(csv_dir):
+        tmp = tempfile.NamedTemporaryFile(mode='r+')
+        with open(os.path.join(csv_dir, csv), 'r') as i:
+            i.next()
+            for line in i:
+               tmp.write(line.rstrip()+"\n")
+        tmp.seek(0)
+        with open(os.path.join(csv_dir, csv), 'w') as o:
+            for line in tmp:
+                o.write(line)
+
+
 def add_frequency_to_csvs():
     for wordcounts in os.listdir(csv_dir):
         with open(os.path.join(csv_dir, wordcounts), 'r') as f:
@@ -34,30 +63,19 @@ def add_frequency_to_csvs():
             print line.rstrip() + ',' + str(float(word_count)/total_words)
         new_file.close()
 
-for person in os.listdir(corpus_dir):
-    speech_text = ""
-    for raw_speech in os.listdir(os.path.join(corpus_dir, person)):
-        if not (raw_speech.startswith(".DS_Store")):
-            speech_text += " " + clean_speech_file_text(person, raw_speech)
-    speech_words = speech_text.lower().split(" ")
-    word_counts = Counter(speech_words)
-    csv_name = person.lower() + ".csv"
-    writefile = open(os.path.join(csv_dir, csv_name), 'wb')
-    writer = csv.writer(writefile)
-    for key, count in word_counts.iteritems():
-        writer.writerow([key, count])
-    writefile.close()
 
-# remove first line, which counts nulls
-for csv in os.listdir(csv_dir):
-    tmp = tempfile.NamedTemporaryFile(mode='r+')
-    with open(os.path.join(csv_dir, csv), 'r') as i:
-        i.next()
-        for line in i:
-           tmp.write(line.rstrip()+"\n")
-    tmp.seek(0)
-    with open(os.path.join(csv_dir, csv), 'w') as o:
-        for line in tmp:
-            o.write(line)
+def add_header():
+    header = 'word,count,relative_use'
+    for wordcount_file in os.listdir(csv_dir):
+        with open(os.path.join(csv_dir, wordcount_file), 'r+') as csv_in:
+            content = csv_in.read()
+            csv_in.seek(0, 0)
+            csv_in.write(header.rstrip('\r\n') + '\n' + content)
 
-add_frequency_to_csvs()
+def main():
+    get_counts()
+    remove_null_count()
+    add_frequency_to_csvs()
+
+
+main()
